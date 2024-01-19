8201c88963f64d98630834c507f172961a6796ba
HaukeFalcon <156836594+HaukeFalcon@users.noreply.github.com>
2024-01-19 10:57:46 -0600

    Add files via upload

--- FUNNEL_2.py
+++ FUNNEL_2.py
@@ -48,6 +48,34 @@ st.write(df3)
 
 
 
+import pandas as pd
+
+df = pd.DataFrame({
+  "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+  "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
+  "Number Eaten": [2, 1, 3, 1, 3, 2],
+})
+
+
+
+
+# Graph Objects
+
+import plotly.graph_objects as go
+
+fig = go.Figure()
+for contestant, group in df.groupby("Contestant"):
+    fig.add_trace(go.Bar(x=group["Fruit"], y=group["Number Eaten"], name=contestant,
+      hovertemplate="Contestant=%s<br>Fruit=%%{x}<br>Number Eaten=%%{y}<extra></extra>"% contestant))
+fig.update_layout(legend_title_text = "Contestant")
+fig.update_xaxes(title_text="Fruit")
+fig.update_yaxes(title_text="Number Eaten")
+fig.show()
+
+
+
+
+
 import streamlit as st
 import pandas as pd
 import numpy as np
