1f800a4e161f5067d05c4fc7307830985cbf11cd
HaukeFalcon <156836594+HaukeFalcon@users.noreply.github.com>
2024-01-23 09:06:19 -0600

    Add files via upload

--- FUNNEL_2.py
+++ FUNNEL_2.py
@@ -12,10 +12,10 @@ pip.main(['install', 'plotly'])
 pip.main(['install', 'plotly_express'])
 
 
-pip.main(['install', 'openpyxl'])
+#pip.main(['install', 'openpyxl'])
 
 
-excel=pd.read_excel('APROBADOS.xlsx',engine = 'openpyxl')
+#excel=pd.read_excel('APROBADOS.xlsx',engine = 'openpyxl')
 
 
 #import plotly.express as px
@@ -26,7 +26,7 @@ import numpy as np
 st.title("FUNNEL ACTIVACASH 2.0 :sunglasses:")
 #df=pd.read_excel('PROCESOS_PIVOTE.xlsx')
 
-st.write(excel)
+#st.write(excel)
 
 
 #df_2=pd.read_csv('APROBADOS.csv')
@@ -126,3 +126,46 @@ fig_2 = px.bar(df, x="Fruit", y="Number Eaten", color="Contestant", barmode="gro
 #fig_2.show()
 st.plotly_chart(fig_2)
 
+
+
+
+
+###########
+###########
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
